c3355ff40bcdb91a74cff4777728706d8a0d0305
Rucha <rucha.kasture2002@gmail.com>
2021-07-12 16:43:07 +0530

    added mcq

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-window.onload=function(){
-    display(0);
-}
 let mcq =[
     {
-        id:q1,
+        id:'q1',
         question:"Which one is the fruit?",
         answer:"apple",
         options:[
@@ -14,7 +11,7 @@ let mcq =[
         ]
     },
     {
-        id:q2,
+        id:'q2',
         question:"Which one is the vegetable?",
         answer:"cabbage",
         options:[
@@ -25,16 +22,33 @@ let mcq =[
         ]
     }
 ]
-     
+
+window.onload=function(){
+    display(0);
+}
 let countIncr = 0;
 function next(){
     countIncr++;
     display(countIncr);
 }
+let countDecr=1;
+function prev(){
+    countDecr--;
 
+    display(countDecr);
+}
 
 function display(count)
 {
-    let q = document.getElementById('section');
+    let q = document.getElementById('problem');
+    let a1 = document.getElementById('choice1');
+    let a2 = document.getElementById('choice2');
+    let a3 = document.getElementById('choice3');
+    let a4 = document.getElementById('choice4');
     q.innerHTML= "<h2>" + mcq[count].question + "</h2>";
+    a1.innerHTML="<p>"+ mcq[count].options[0] +"</p>" 
+    a2.innerHTML="<p>"+ mcq[count].options[1] +"</p>" 
+    a3.innerHTML="<p>"+ mcq[count].options[2] +"</p>" 
+    a4.innerHTML="<p>"+ mcq[count].options[3] +"</p>" 
+    
 }
